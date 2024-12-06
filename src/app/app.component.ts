f795dff9429698a04af2b226e69e7c1c89453182
Germán Morales jr <german.morales01@alumnos.ucn.cl>
2024-12-06 17:14:47 -0300

    feat: add html template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { OnInit } from '@angular/core';
 import { initFlowbite } from 'flowbite';
+import { ContactFormComponent } from './components/contact-form/contact-form.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { SkillsComponent } from './components/skills/skills.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  imports: [ContactFormComponent, HeaderComponent, ProjectsComponent, SkillsComponent],
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
--- src/app/components/contact-form/contact-form.component.ts
+++ src/app/components/contact-form/contact-form.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-contact-form',
-  imports: [],
   templateUrl: './contact-form.component.html',
-  styleUrl: './contact-form.component.css'
+  styleUrls: ['./contact-form.component.css']
 })
 export class ContactFormComponent {
-
+  sendMessage() {
+    alert('Formulario enviado con éxito (simulación).');
+  }
 }
